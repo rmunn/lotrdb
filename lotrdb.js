934c9391c05ce8bc78d8eea1e5c9f502e7e23f82
Dirk Meijer <d.w.j.meijer@gmail.com>
2015-04-13 07:22:03 +0200

    localstorage things

--- lotrdb.js
+++ lotrdb.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var app = angular.module('deckbuilder', []);
+  var app = angular.module('deckbuilder', ['ngStorage']);
   
   
   app.factory('getData', function($http) {
@@ -26,7 +26,7 @@
   
   
   //Logic for the pack selection
-  app.controller('packSelect',["filtersettings",function(filtersettings){
+  app.controller('packSelect',["filtersettings","$localStorage",function(filtersettings,$localStorage){
     this.filtersettings=filtersettings;
     this.full=["core", "kd", "hon", "tvoi", "tlr", "ohuh", "thfg", "trg", "tsf", "tdt", "twoe", "otd", "catc", "rtr", "tdf", "ttt", "efmg", "tbr", "ajtr", "twitw", "eaad", "tit", "trd", "thoem", "tld", "aoo", "tnie", "tdm", "fos", "tbog", "cs", "rtm", "saf", "tmv", "tac"]; //all expansions so far
     this.toggle=function(exp){
@@ -36,12 +36,15 @@
       } else {
         this.filtersettings.pack.splice(ind,1);
       }
+      $localStorage.pack = this.filtersettings.pack;
     };
     this.selectNone=function(){
       this.filtersettings.pack=[];
+      $localStorage.pack = this.filtersettings.pack;
     };
     this.selectAll=function(){
       this.filtersettings.pack=this.full.slice(0); //make a clone
+      $localStorage.pack = this.filtersettings.pack;
     };
   }]);
   
@@ -49,8 +52,8 @@
     return {
       restrict: 'E',
       templateUrl: 'packs.html',
-      //controller: 'packSelect',
-      //controllerAs: 'packs'
+      controller: 'packSelect',
+      controllerAs: 'packs'
     };
   });
   
@@ -77,6 +80,13 @@
       templateUrl: 'header.html'
     };
   });
+  //Page footer
+  app.directive('myfooter', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'footer.html'
+    };
+  });
   
   app.filter('cardfilter', function(){
     return function (input, scope) {
@@ -91,13 +101,13 @@
     };
   });
   
-  app.factory('filtersettings', function () {
+  app.factory('filtersettings',["$localStorage",function ($localStorage) {
     var filtersettings={};
-    filtersettings.pack=["core"];
+    filtersettings.pack= $localStorage.pack || ["core"];
     filtersettings.type={'1hero': true, '2ally': false, '3attachment': false, '4event': false, '5quest': false};
     filtersettings.spheres={'1leadership': true, '4lore': true, '3spirit': true, '2tactics': true, '5neutral': true, '6baggins':false, '7fellowship':false};
     return filtersettings;
-  });
+  }]);
   
   
   //Logic for the card selection
@@ -224,6 +234,10 @@
       return deck.countAllies()+deck.countAttachments()+deck.countEvents()+deck.countQuests();
     }
     
+    deck.empty = function() {
+      return (deck.countAllies()+deck.countAttachments()+deck.countEvents()+deck.countQuests()+deck.countHeroes())==0;
+    }
+    
     return deck;
   });
   
@@ -250,8 +264,12 @@
   app.factory('image',function(){
     var image={};
     image.url="";
+    image.name="";
+    image.exp="";
     image.update = function(card){
       image.url = card.img;
+      image.name = card.name;
+      image.exp = card.exp;
     }
     image.getUrl = function(){
       return image.url;
@@ -260,11 +278,14 @@
   });
   
   
-  app.controller('cardPreview',['$scope','image',function($scope,image){
+  app.controller('cardPreview',['$scope','image','translate',function($scope,image,translate){
     this.image=image;
     this.getImg = function() {
       return this.image.getUrl();
     };
+    this.name = function() {
+      return image.name + " (" + translate[image.exp] +")";
+    }
   }]);
   
   app.directive('cardpreview', function() {
@@ -274,6 +295,127 @@
       controller: 'cardPreview',
       controllerAs: 'preview'
     };
+  });
+  
+  
+  
+  
+  app.controller('myDecks',['deck','$localStorage','translate',function(deck,$localStorage,translate){
+    if (!$localStorage.decks){
+      $localStorage.decks={};
+    }
+    this.decks = $localStorage.decks;
+    this.deckname="";
+    this.saveDeck = function(deckname) {
+      if (deck.empty()) {
+        return alert('Deck is empty!');
+      };
+      if (this.deckname=="") {
+        return alert('Please enter a name!');
+      };
+      if ($localStorage.decks[deckname]!=null){
+        if (confirm('A deck by that name exists, overwrite?')) {
+        } else{
+          return 0;
+        }
+      }
+      var newdeck = {};
+      newdeck.deck = {};
+        newdeck.deck["1hero"] = deck["1hero"].slice(0);
+        newdeck.deck["2ally"] = deck["2ally"].slice(0);
+        newdeck.deck["3attachment"] = deck["3attachment"].slice(0);
+        newdeck.deck["4event"] = deck["4event"].slice(0);
+        newdeck.deck["5quest"] = deck["5quest"].slice(0);
+      newdeck.deckname = deckname;
+      newdeck.date = Date.now();
+      $localStorage.decks[deckname] = newdeck;
+    };
+    this.loadDeck = function(deckname) {
+      deck["1hero"] = $localStorage.decks[deckname].deck["1hero"].slice(0);
+      deck["2ally"] = $localStorage.decks[deckname].deck["2ally"].slice(0);
+      deck["3attachment"] = $localStorage.decks[deckname].deck["3attachment"].slice(0);
+      deck["4event"] = $localStorage.decks[deckname].deck["4event"].slice(0);
+      deck["5quest"] = $localStorage.decks[deckname].deck["5quest"].slice(0);
+      this.deckname = deckname;
+    };
+    this.deleteDeck = function(deckname) {
+      if (confirm('Are you sure you want to delete this deck?')) {
+        delete $localStorage.decks[deckname];
+      };
+    };
+    this.clearDeck = function() {
+      //if (deck.empty() || confirm('Are you sure you want to clear this deck?')) {
+        deck["1hero"] = [];
+        deck["2ally"] = [];
+        deck["3attachment"] = [];
+        deck["4event"] = [];
+        deck["5quest"] = [];
+        this.deckname = "";
+      //};
+    };
+  }]);
+  
+  
+  
+  app.directive('mydecks', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'mydecks.html',
+      controller: 'myDecks',
+      controllerAs: 'mydecks'
+    };
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  app.factory('translate',function(){
+    var translate={};
+    translate[""]="";
+    translate.core="Core Set";
+    translate.kd="Khazad-Dûm";
+    translate.hon="Heirs of Númenor";
+    translate.tvoi="The Voice of Isengard";
+    translate.tlr="The Lost Realm";
+    translate.ohuh="Over Hill and Under Hill";
+    translate.thfg="The Hunt for Gollum";
+    translate.trg="The Redhorn Gate";
+    translate.tsf="The Steward's Fear";
+    translate.tdt="The Dunland Trap";
+    translate.twoe="The Wastes of Eriador";
+    translate.otd="On the Doorstep";
+    translate.catc="Conflict at the Carrock";
+    translate.rtr="Road to Rivendell";
+    translate.tdf="The Drúadan Forest";
+    translate.ttt="The Three Trials";
+    translate.efmg="Escape from Mount Gram";
+    translate.tbr="The Black Riders";
+    translate.ajtr="A Journey to Rhosgobel";
+    translate.twitw="The Watcher in the Water";
+    translate.eaad="Encounter at Amon Dîn";
+    translate.tit="Trouble in Tharbad";
+    translate.trd="The Road Darkens";
+    translate.thoem="The Hills of Emyn Muil";
+    translate.tld="The Long Dark";
+    translate.aoo="Assault on Osgiliath";
+    translate.tnie="The Nin-in-Eiilph";
+    translate.tdm="The Dead Marshes";
+    translate.fos="Foundations of Stone";
+    translate.tbog="The Blood of Gondor";
+    translate.cs="Celebrimbor's Secret";
+    translate.rtm="Return to Mirkwood";
+    translate.saf="Shadow and Flame";
+    translate.tmv="The Morgul Vale";
+    translate.tac="The Antlered Crown";
+    translate.ttos="The Treason of Saruman";
+    return translate;
   });
   
 
